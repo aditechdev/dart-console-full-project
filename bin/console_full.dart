b2dc1ccc6ee19f0e89b572ec381c3487c314cb9e
aditechdev <aditechdevofficial@gmail.com>
2022-06-30 20:25:03 +0530

    :fire: Print SUm

--- bin/console_full.dart
+++ bin/console_full.dart
@@ -1,5 +1,10 @@
 import 'package:console_full/console_full.dart' as console_full;
 
 void main(List<String> arguments) {
-  print('Hello world: ${console_full.calculate()}!');
+  try {
+    print(
+        'Hello world: ${console_full.calculate()}!\nArgs Sum: ${arguments.fold<int>(0, (previousValue, element) => previousValue + int.parse(element))}');
+  } catch (e) {
+    print(e);
+  }
 }
